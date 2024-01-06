036efeecf800a7f59afc2824db17577cee9ea063
mac_a2141 <victor@piousbox.com>
2024-01-06 17:02:50 -0500

    autobuild 20240106

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,9 @@
 
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile += %w( wco_email/application.js wco_email/application.css )
+Rails.application.config.assets.precompile += %w(
+  wco/application.css
+
+  wco_email/application.js
+  wco_email/application.css
+);
