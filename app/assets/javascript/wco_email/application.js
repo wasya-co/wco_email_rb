62e2b6a561e0cf424b1d25e35bcec48611ead6d6
mac_a2141 <victor@piousbox.com>
2024-02-10 10:01:31 -0600

    autobuild `date "+%Y%m%d"`

--- app/assets/javascript/wco_email/application.js
+++ app/assets/javascript/wco_email/application.js
@@ -5,6 +5,11 @@
 //= require ./conversations
 //
 
+function resizeIframe(obj) {
+  obj.style.height = '100px';
+  obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 10 + 'px';
+}
+
 $(function () {
 
 
