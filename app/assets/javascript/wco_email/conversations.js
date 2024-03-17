7b9b131683de65fc9793f104555e3d1121abcf00
mac_a2141 <victor@piousbox.com>
2024-03-17 16:28:52 -0500

    autobuild `date "+%Y%m%d"`

--- app/assets/javascript/wco_email/conversations.js
+++ app/assets/javascript/wco_email/conversations.js
@@ -1,45 +1,6 @@
 $(function() {
 
 
-/*
- * select-all
- * _vp_ 2023-02-28
-**/
-$("input[type='checkbox'].i-sel").change((ev) => {
-  // logg(ev, 'changed-112')
-
-  $( $(".n-selected")[0] ).html( $("input[type='checkbox'].i-sel:checked").length )
-})
-$("th.select-all input[type='checkbox']").change((e) => {
-  const count = $("input[type='checkbox'].i-sel:checked").length
-  const new_state = count ? false : true // all will be checked?
-
-  $(".select-all input[type='checkbox']").prop('checked', new_state)
-
-  $( $("input[type='checkbox'].i-sel") ).each( i => {
-    $( $("input[type='checkbox'].i-sel")[i] ).prop('checked', new_state)
-  })
-
-  $( $(".n-selected")[0] ).html( $("input[type='checkbox'].i-sel:checked").length )
-})
-
-
-
-// $("body.email_conversations-show .preview-btn").click(function(e) {
-//   // logg($(this).data(), 'clicked')
-//   var msgId = $(this).data().msg.id
-//   var msgC = $(`.msg-container[data-msg-id='${msgId}']`) // msgContainer
-//   if ($(msgC).data('expanded')) {
-//     $(msgC).data('expanded', false)
-//     msgC.find(".to-expand").css('display', 'none')
-//   } else {
-//     $(msgC).data('expanded', true)
-//     msgC.find(".to-expand").css('display', 'block')
-//   }
-// })
-
-
-
 $(".archive-btn").click(function(e) {
   if ( !confirm('Are you sure?') ) { return; }
 
