a2f98255941d4a45359ea0b0333c83b54e87da23
mac_a2141 <victor@piousbox.com>
2024-08-01 15:52:49 -0500

    autobuild `date "+%Y%m%d"`

--- app/controllers/wco_email/messages_controller.rb
+++ app/controllers/wco_email/messages_controller.rb
@@ -5,6 +5,17 @@ class WcoEmail::MessagesController < WcoEmail::ApplicationController
 
   def show
     @message = WcoEmail::Message.find params[:id]
+
+    @client ||= Aws::S3::Client.new({
+      region:            ::S3_CREDENTIALS[:region_ses],
+      access_key_id:     ::S3_CREDENTIALS[:access_key_id_ses],
+      secret_access_key: ::S3_CREDENTIALS[:secret_access_key_ses],
+    })
+    stub     = @message.stub
+    raw      = @client.get_object( bucket: stub.bucket, key: stub.object_key ).body.read
+    raw      = raw.encode('utf-8', invalid: :replace, undef: :replace, replace: '_' )
+    @the_mail = Mail.new( raw )
+
     authorize! :show, @message
   end
 
